8918c4df4c3c956be668c938b9e7af9202017aaa
cguasca2019 <51269156+cguasca2019@users.noreply.github.com>
2019-10-22 01:53:56 -0500

    Add files via upload

--- js/core.js
+++ js/core.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
 });
 
 function readData() {
-    $.getJSON('js/data.json',function(data){
-        console.log(data);
-        // var output = '<ul>';  
+    $.getJSON('https://raw.githubusercontent.com/cguasca2019/PruebaFrontEndCG/master/js/data.json',function(data){
+     var oferts = data.offerts;
+     var sliders = data.slides; 
+    
+    console.log(data.offerts);
+        var output = '<ul>';  
         // $.each(data, function(key,val){
         //     output += '<li>'+ val.name + " " + val.zipcode+ '</li>';
         // });
