a1e6947d7e2828c95503a3f195785b989d47582d
cguasca2019 <51269156+cguasca2019@users.noreply.github.com>
2019-10-22 11:08:55 -0500

    JQUERY ADD

--- js/core.js
+++ js/core.js
@@ -4,15 +4,20 @@ $(document).ready(function(){
 
 function readData() {
     $.getJSON('https://raw.githubusercontent.com/cguasca2019/PruebaFrontEndCG/master/js/data.json',function(data){
-     var oferts = data.offerts;
-     var sliders = data.slides; 
-    
-    console.log(data.offerts);
-        var output = '<ul>';  
-        // $.each(data, function(key,val){
-        //     output += '<li>'+ val.name + " " + val.zipcode+ '</li>';
-        // });
-        // output += '</ul>';
-        // $('#update').html(output);
+        // var oferts = data.offerts;
+        // var sliders = data.slides; 
+        var output1 = '';  
+        var output2 = '';  
+        var item = 0;
+        $.each(data.slides, function(key,val){
+            output1 += '<div class="item"><img src="'+ val.url +'" alt="'+ val.name +'"><div class="carousel-caption"> '+ val.name +' </div></div>';
+            output2 += '<li data-target="#carousel1" data-slide-to="' + item +'"></li>'
+            item ++;           
+        });
+        $('.carousel-inner').html(output1);
+        $('.carousel-indicators').html(output2);
+        $('.carousel-inner > div').first().addClass('active');
+        $('.carousel-indicators > li').first().addClass('active');       
+        $('#carousel1').carousel();      
     });
 }
